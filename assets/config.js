f27a4116b0c68df695e2ad0a4d516bc0814d9fee
chokrifk <43779961+chokrifk@users.noreply.github.com>
2026-08-29 01:31:28 +0100

    Change Supabase client initialization variable name
    Updated the Supabase client initialization to use 'dbClient' instead of 'clientSupabase'.

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://iahzasnluqapwppclfmn.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_TIb7eyfTYz5x-DhexGOWDw_VkPja_E-'; // 
+const SUPABASE_KEY = 'sb_publishable_TIb7eyfTYz5x-DhexGOWDw_VkPja_E-'; // Votre clé publishable
 
-// On utilise 'clientSupabase' au lieu de 'supabase' pour éviter tout conflit de nom
-window.clientSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// Initialisation globale sur l'objet window
+window.dbClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
