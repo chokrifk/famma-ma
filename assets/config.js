2156ed541f496b520a9b056e5736adf4609484d6
chokrifk <43779961+chokrifk@users.noreply.github.com>
2026-08-29 01:10:54 +0100

    Rename Supabase client variable to clientSupabase
    Renamed the Supabase client variable to avoid naming conflicts.

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,5 @@
 const SUPABASE_URL = 'https://iahzasnluqapwppclfmn.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_TIb7eyfTYz5x-DhexGOWDw_VkPja_E-'; // Ta clé sb_publishable_...
+const SUPABASE_KEY = 'sb_publishable_TIb7eyfTYz5x-DhexGOWDw_VkPja_E-'; // 
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// On utilise 'clientSupabase' au lieu de 'supabase' pour éviter tout conflit de nom
+window.clientSupabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
