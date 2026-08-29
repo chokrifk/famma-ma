4250c751292ee517f588784d97adc35687e272b4
chokrifk <43779961+chokrifk@users.noreply.github.com>
2026-08-29 01:52:52 +0100

    Refactor Supabase client initialization
    Updated the initialization of the Supabase client to use 'window.db' instead of 'window.dbClient'.

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://iahzasnluqapwppclfmn.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_TIb7eyfTYz5x-DhexGOWDw_VkPja_E-'; // Votre clé publishable
+const SUPABASE_KEY = 'sb_publishable_TIb7eyfTYz5x-DhexGOWDw_VkPja_E-'; // Ta clé publishable
 
-// Initialisation globale sur l'objet window
-window.dbClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+// On utilise window.db pour éviter tout conflit de nom 'supabase'
+window.db = window.supabase ? window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY) : null;
